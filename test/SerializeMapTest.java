c68d0ce45aac439e8535894112b1255663e9fe52
okuyamaoo <okuyamaoo@4f952920-8142-4fd5-8b69-023f7afe8796>
2011-06-16 05:24:40 +0000

    20110617-2

---
+++ test/SerializeMapTest.java
@@ -0,0 +1,75 @@
+package test;
+
+import java.util.*;
+import java.util.concurrent.locks.*;
+
+import okuyama.imdst.util.serializemap.*;
+
+
+public class SerializeMapTest extends Thread {
+
+    private static boolean status = true;
+
+    private static SerializeMap testMap = new SerializeMap(1000000, 900000, 100000);
+
+    public static ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    public static Lock r = rwl.readLock();
+    public static Lock w = rwl.writeLock();
+
+    private int threadPrefix = 0;
+
+
+    public SerializeMapTest(int prefix) {
+        threadPrefix = prefix;
+    }
+
+    public static void main(String[] args) {
+        try {
+            long totalExecCount = 0L;
+            int maxThreads = Integer.parseInt(args[0]);
+            w.lock();
+            SerializeMap initMap = new SerializeMap(100, 90, 10);
+            initMap.put("a", "b");
+            initMap.get("a");
+            initMap = null;
+
+            SerializeMapTest[] tList = new SerializeMapTest[maxThreads];
+            
+            for (int idx = 0; idx < maxThreads; idx++) {
+                SerializeMapTest me = new SerializeMapTest(idx);
+                me.start();
+                tList[idx] = me;
+            }
+            
+            w.unlock();
+            Thread.sleep(60000);
+            SerializeMapTest.status = false;
+
+            for (int idx = 0; idx < maxThreads; idx++) {
+                tList[idx].join();
+            }
+            System.out.println("TotalExecCount = " + testMap.size());
+            System.out.println("QPS = " + (testMap.size() / 60));
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run () {
+        try {
+            r.lock();
+            r.unlock();
+            String key = "KeyABCDEFG0123456789_" + threadPrefix + "_";
+            String value = "Value123456789ABCDEFGHIJKLMNOPQRSTUWXYZABCDEFGHIJKLMNOPQRSTUWXYZ_" + threadPrefix + "_";
+          
+            for (int idx = 0; SerializeMapTest.status; idx++) {
+                //System.out.println(key + idx + "  " + value + idx);
+                testMap.put(key + idx, value + idx);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
