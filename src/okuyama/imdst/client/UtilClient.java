251d99e9da32c9a70e55b1276d40262d42b2e61b
okuyamaoo <okuyamaoo@4f952920-8142-4fd5-8b69-023f7afe8796>
2011-07-02 05:52:34 +0000

    20110702-2

--- src/okuyama/imdst/client/UtilClient.java
+++ src/okuyama/imdst/client/UtilClient.java
@@ -24,31 +24,44 @@ public class UtilClient {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("args[0]=Command");
-            System.out.println("Command1. DataExport args1=dataexport args2=DataNode-IPAdress args3=DataNode-Port");
+            System.out.println("Command1. DataExport args1=bkup args2=DataNode-IPAdress args3=DataNode-Port");
             System.out.println("Command2. TruncateData args1=truncatedata args2=MainMasterNode-IPAdress args3=MainMasterNode-Port args4=IsolationName or 'all'");
+            System.out.println("Command3. MasterNodeConfigCheck args1=masterconfig args2=MainMasterNode-IPAdress args3=MainMasterNode-Port");
+            System.exit(1);
         }
 
-        if (args[0].equals("dataexport") || args[0].equals("bkup")) {
+        if (args[0].trim().equals("dataexport") || args[0].trim().equals("bkup")) {
             if (args.length != 3) {
-                System.out.println("args[0]=Command, args[1]=serverip, args[2]=port");
+                System.out.println("Argument Error! args[0]=dataexport or bkup, args[1]=serverip, args[2]=port");
                 System.exit(1);
             }
 
             dataExport(args[1], Integer.parseInt(args[2]));
         }
 
-        if (args[0].equals("truncatedata") || args[0].equals("truncatedata")) {
+        if (args[0].trim().equals("truncatedata")) {
             if (args.length != 4) {
-                System.out.println("args[0]=Command, args[1]=MainMasterNodeServerIp, args[2]=port, args[3]=IsolationPrefix or 'all'");
+                System.out.println("Argument Error! args[0]=truncatedata, args[1]=MainMasterNodeServerIp, args[2]=port, args[3]=IsolationPrefix or 'all'");
                 System.exit(1);
             }
 
             truncateData(args[1], Integer.parseInt(args[2]), args[3]);
         }
 
+
+        if (args[0].trim().equals("masterconfig")) {
+            if (args.length != 3) {
+                System.out.println("Argument Error! args[0]=masterconfig, args[1]=MainMasterNodeServerIp, args[2]=port");
+                System.exit(1);
+            }
+
+            masterNodeConfigCheck(args[1], Integer.parseInt(args[2]));
+        }
+
+
         if (args[0].equals("fulldataexport")) {
             if (args.length != 3) {
-                System.out.println("args[0]=Command, args[1]=serverip, args[2]=port");
+                System.out.println("Argument Error! args[0]=Command, args[1]=serverip, args[2]=port");
                 System.exit(1);
             }
 
@@ -123,4 +136,35 @@ public class UtilClient {
         }
     }
 
+
+    public static void masterNodeConfigCheck (String serverip, int port) {
+        Socket socket = null;
+        PrintWriter pw = null;
+        BufferedReader br = null;
+
+        try {
+            socket = new Socket();
+            InetSocketAddress inetAddr = new InetSocketAddress(serverip, port);
+            socket.connect(inetAddr, 10000);
+            socket.setSoTimeout(10000);
+            pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8")));
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+            pw.println("998");
+            pw.flush();
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null) socket.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
 }
