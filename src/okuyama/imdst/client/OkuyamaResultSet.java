f355ec7a6985dd35c7b0f3768417c91d8a618759
okuyamaoo <okuyamaoo@4f952920-8142-4fd5-8b69-023f7afe8796>
2011-10-15 07:36:46 +0000

    20111015-3

--- src/okuyama/imdst/client/OkuyamaResultSet.java
+++ src/okuyama/imdst/client/OkuyamaResultSet.java
@@ -6,7 +6,7 @@ package okuyama.imdst.client;
  * 以下のような構文にてデータを取得する
  * 以下は、Tagを利用して全ての紐付くKeyとValueを出力している
  * -----------------------------------------------------------------
- * OkuyamaResultSet resultSet = client.getTagKeyResult(tagStr);
+ * OkuyamaResultSet resultSet = client.getTagKeysResult(tagStr);
  * 
  * while(resultSet.next()) {
  *     System.out.println("Key=" + (Object)resultSet.getKey());
@@ -21,13 +21,45 @@ package okuyama.imdst.client;
  */ 
 public interface OkuyamaResultSet {
 
+    /**
+     * データ取得前に呼び出すことで取得位置を1つ進める.<br>
+     * OkuyamaResultSetが返された直後はカーソル位置がデータの最初の場所にないため、まず呼び出さなければデータは取得できない<br>
+     * カーソルが終端に達し場合はfalseが返却されカーソルが終端ではない場合はtrueが返される
+     *
+     * @return boolean  カーソルが終端に達し場合はfalse  カーソルが終端ではない場合はtrue
+     * @throws OkuyamaClientException なんだかの理由でカーソルの移動に失敗した場合
+     */
     public boolean next() throws OkuyamaClientException;
 
+    /**
+     * 現在のカーソル位置のKey値を取得する.<br>
+     *
+     * @return Object Key値
+     * @throws OkuyamaClientException なんだかの理由でKey値の取得に失敗した場合
+     */
     public Object getKey() throws OkuyamaClientException;
 
+    /**
+     * 現在のカーソル位置のValue値を取得する.<br>
+     *
+     * @return Object Value値
+     * @throws OkuyamaClientException なんだかの理由でValue値の取得に失敗した場合
+     */
     public Object getValue() throws OkuyamaClientException;
 
+    /**
+     * ResultSetを終了する.<br>
+     * 利用後は必ず呼び出してリソースを解放する必要がある<br>
+     *
+     * @throws OkuyamaClientException なんだかの理由でリソースの解放に失敗した場合
+     */
     public void close() throws OkuyamaClientException;
 
+    /**
+     * このResultSetが終了しているかを確認する.<br>
+     *
+     * @return boolean true=既に終了している false=終了していない
+     * @throws OkuyamaClientException なんだかの理由でリソースの終了確認に失敗した場合
+     */
     public boolean isClose() throws OkuyamaClientException;
 }
