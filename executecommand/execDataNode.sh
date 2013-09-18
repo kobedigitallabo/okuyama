60b54beec5a0ae2e76df30c4ca19f842a7799155
okuyamaoo <okuyamaoo@4f952920-8142-4fd5-8b69-023f7afe8796>
2013-09-18 08:59:08 +0000

    install配下のokuyama.jarを最新化

--- executecommand/execDataNode.sh
+++ executecommand/execDataNode.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -classpath ./classes:./lib/log4j-1.2.14.jar:./lib/javamail-1.4.1.jar:./lib/commons-codec-1.4.jar -Xmx368m -Xms256m  -server -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseParNewGC okuyama.base.JavaMain  /Main.properties /DataNode.properties
+java -classpath ./classes:./lib/log4j-1.2.14.jar:./lib/javamail-1.4.1.jar:./lib/commons-codec-1.4.jar -Xmx1512m -Xms512m -Xmn128m -server -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseParNewGC okuyama.base.JavaMain  /Main.properties /DataNode_small.properties
