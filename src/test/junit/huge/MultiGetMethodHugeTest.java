2a3e1c9ddfb97c69a524b4642e799b58e8463982
kdl-s-ito <s-ito@kdl.co.jp>
2014-07-03 11:09:26 +0900

    巨大データに対するgetメソッドによる複数同時取得の簡単なテスト実装

---
+++ src/test/junit/huge/MultiGetMethodHugeTest.java
@@ -0,0 +1,86 @@
+package test.junit.huge;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import okuyama.imdst.client.OkuyamaClient;
+import okuyama.imdst.client.OkuyamaClientException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import test.junit.MethodTestHelper;
+
+/**
+ * 巨大データに対するgetメソッドによる複数同時取得の簡単なテスト。
+ * @author s-ito
+ *
+ */
+public class MultiGetMethodHugeTest {
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	private static MethodTestHelper helper = new MethodTestHelper();
+
+	private OkuyamaClient okuyamaClient;
+
+	private String[] testDataKey;
+
+	private String[] testDataValue;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		MultiGetMethodHugeTest.helper.init();
+		MultiGetMethodHugeTest.helper.initTestData();
+		// okuyamaに接続
+		this.okuyamaClient =  MultiGetMethodHugeTest.helper.getConnectedOkuyamaClient();
+		// テストデータを設定
+		String testKey = MultiGetMethodHugeTest.helper.createTestDataKey(true);
+		String testValue = MultiGetMethodHugeTest.helper.createTestDataValue(true);
+		this.testDataKey = new String[] {
+				testKey + 1, testKey + 2, testKey + 3
+		};
+		this.testDataValue = new String[] {
+				testValue + 1, testValue + 2, testValue + 3
+		};
+		this.okuyamaClient.setValue(this.testDataKey[0], this.testDataValue[0]);
+		this.okuyamaClient.setValue(this.testDataKey[1], this.testDataValue[1]);
+		this.okuyamaClient.setValue(this.testDataKey[2], this.testDataValue[2]);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// テストデータを破棄
+		try {
+			this.okuyamaClient.removeValue(this.testDataKey[0]);
+			this.okuyamaClient.removeValue(this.testDataKey[1]);
+			this.okuyamaClient.removeValue(this.testDataKey[2]);
+		} catch (OkuyamaClientException e) {
+		}
+		this.okuyamaClient.close();
+	}
+
+	@Test
+	public void キーに対応した値を取得する() throws Exception {
+		Map<?, ?> result = this.okuyamaClient.getMultiValue(this.testDataKey);
+		assertEquals(result.get(this.testDataKey[0]), this.testDataValue[0]);
+		assertEquals(result.get(this.testDataKey[1]), this.testDataValue[1]);
+		assertEquals(result.get(this.testDataKey[2]), this.testDataValue[2]);
+	}
+
+}
