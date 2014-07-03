e280428184d9f5dccab2c60b75f18ce15c71bfbd
kdl-s-ito <s-ito@kdl.co.jp>
2014-07-03 13:39:34 +0900

    巨大データに対するgetメソッドの繰り返しテストを実装しました。

---
+++ src/test/junit/iteration/huge/GetMethodIterationHugeTest.java
@@ -0,0 +1,75 @@
+package test.junit.iteration.huge;
+
+import static org.junit.Assert.*;
+import okuyama.imdst.client.OkuyamaClient;
+import okuyama.imdst.client.OkuyamaClientException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import test.junit.MethodTestHelper;
+
+/**
+ * 巨大データに対するgetメソッドの繰り返しテスト。
+ * @author s-ito
+ *
+ */
+public class GetMethodIterationHugeTest {
+
+	private static MethodTestHelper helper = new MethodTestHelper();
+
+	private OkuyamaClient okuyamaClient;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		GetMethodIterationHugeTest.helper.init();
+		GetMethodIterationHugeTest.helper.initBigTestData();
+		// okuyamaに接続
+		this.okuyamaClient =  GetMethodIterationHugeTest.helper.getConnectedOkuyamaClient();
+		// テストデータを設定
+		for (int i = 0;i < 5000;i++) {
+			String testDataKey = GetMethodIterationHugeTest.helper.createTestDataKey(true, i);
+			String testDataValue = GetMethodIterationHugeTest.helper.createTestDataValue(true, i);
+			this.okuyamaClient.setValue(testDataKey, testDataValue);
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// テストデータを破棄
+		try {
+			for (int i = 0;i < 5000;i++) {
+				String testDataKey = GetMethodIterationHugeTest.helper.createTestDataKey(true, i);
+				this.okuyamaClient.removeValue(testDataKey);
+			}
+		} catch (OkuyamaClientException e) {
+		}
+		this.okuyamaClient.close();
+	}
+
+	@Test
+	public void キーに対応した値を取得する() throws Exception {
+		for (int i = 0;i < 5000;i++) {
+			String testDataKey = GetMethodIterationHugeTest.helper.createTestDataKey(true, i);
+			String testDataValue = GetMethodIterationHugeTest.helper.createTestDataValue(true, i);
+			String[] result = this.okuyamaClient.getValue(testDataKey);
+			if (result[0].equals("true")) {
+				assertEquals(result[1], testDataValue);
+			} else {
+				fail("getメソッドエラー");
+			}
+		}
+	}
+
+}
